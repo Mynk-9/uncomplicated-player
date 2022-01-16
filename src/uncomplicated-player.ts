bf10fb6ec19fc0c5dea64bf5c91fb9c00aa69191
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-01-16 18:59:43 +0530

    Added details of player mechanism

--- src/uncomplicated-player.ts
+++ src/uncomplicated-player.ts
@@ -8,6 +8,30 @@ interface UncomplicatedPlayer {
     set prefetch(prefetchVal: boolean);
 }
 
+/**
+ * Player mechanism:
+ * We use multiple MediaElementAudioSourceNode in an array and cycle over them
+ * for tracks. Array size is determined my prefetch size which in turn
+ * determines seek length of the queue.
+ * This prefetch array helps in smoother audio transitions when switching
+ * tracks as all the tracks in the array have their source set and hence a
+ * buffer of data ready.
+ * When we go next on the player, essentially the current player index is
+ * updated with +1 mod length of track. And the other indexes are adjusted
+ * accordingly.
+ *
+ * Explanation with diagram:
+ *
+ *  track1, track2, track3                  track4, track2, track3
+ * [node_1, node_2, node_3]     next       [node_1, node_2, node_3]
+ *    0       1       2       ------->        0       1        2
+ *  prev     curr    next                    next    prev     curr
+ *
+ * Here we essentially changed the source of node_1 from track1 to track4 and
+ * change our curr/next/prev indexes.
+ * In this player, we expand this logic to involve more than 3 nodes.
+ */
+
 const UncomplicatedPlayer = (() => {
     /// instance of the player
     let instance: UncomplicatedPlayer;
